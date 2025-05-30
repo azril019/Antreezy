399835d74e1ea553218344cbb37f21be5a8e5886
azril 019 <azril.muhammad0001@gmail.com>
2025-05-30 13:44:27 +0700

    feat: implement user authentication and profile management
    - Added user registration, login, and profile retrieval APIs.
    - Introduced JWT for authentication and cookie management.
    - Implemented error handling and validation using Zod.
    - Updated package dependencies for bcrypt, jsonwebtoken, and mongodb.

---
+++ antreezy/src/app/api/profile/route.ts
@@ -0,0 +1,30 @@
+import errHandler from "@/helpers/errHandler";
+import UserModel from "@/db/models/UserModel";
+
+export async function GET(request: Request) {
+  try {
+    const userId = request.headers.get("x-user-id");
+    console.log("🚀 ~ GET ~ user:", userId);
+
+    if (!userId) {
+      throw { status: 400, message: "User ID header is missing" };
+    }
+
+    const userLogin = await UserModel.getProfile(userId);
+
+    if (!userLogin) {
+      throw { status: 404, message: "User not found" };
+    }
+
+    return Response.json({
+      user: {
+        id: userLogin._id.toString(),
+        email: userLogin.email,
+        username: userLogin.username,
+        role: userLogin.role,
+      },
+    });
+  } catch (error) {
+    return errHandler(error);
+  }
+}
